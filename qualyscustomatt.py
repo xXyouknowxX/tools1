d17530f1970ec4ddad844098cd82c0e622a803f8
xXyouknowxX <101567079+xXyouknowxX@users.noreply.github.com>
2024-01-11 10:36:27 +0100

    Add files via upload

--- qualyscustomatt.py
+++ qualyscustomatt.py
@@ -1,6 +1,24 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
+
+def get_asset_id(username, password, ip_address):
+    # Define the URL to fetch asset information
+    url = 'https://qualysapi.qg4.apps.qualys.com/api/2.0/fo/asset/host/'  # Update with the correct API endpoint
+    params = {
+        'action': 'list',
+        'ips': ip_address,
+        'details': 'Basic'
+    }
+    response = requests.get(url, params=params, auth=HTTPBasicAuth(username, password))
+    if response.status_code == 200:
+        data = response.json()
+        # Extract and return the asset ID from the response
+        # Note: Update the JSON path according to the actual structure of the response
+        return data['RESPONSE']['ASSET_LIST']['ASSET']['ID']
+    else:
+        return None
+
 def update_qualys_asset_by_ip(username, password, asset_ip, custom_attributes):
     url = 'https://qualysapi.qg4.apps.qualys.com/qps/rest/2.0/update/am/asset'
     headers = {
